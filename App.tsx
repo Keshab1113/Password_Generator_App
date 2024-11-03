3ec1adf3c8ab78f9d3717a6defbdf5cfd9c3ca29
Keshab Das <110785343+Keshab1113@users.noreply.github.com>
2024-11-03 16:39:54 +0530

    Random background color added

--- App.tsx
+++ App.tsx
@@ -1,15 +1,29 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native'
+import React, { useState } from 'react'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import GeneratePassword from './components/GeneratePassword'
 
 
 const App = () => {
+  const [randomBackground, setRandomBackground] = useState("#ffffff");
+  const generateColor = () => {
+    const hexRange = "0123456789ABCDEF";
+    let color = "#";
+    for (let i = 0; i < 6; i++){
+      color+=hexRange[Math.floor(Math.random()*16)]
+    }
+    setRandomBackground(color);
+  }
   return (
-    <View style={styles.container}>
+    <View style={[styles.container, {backgroundColor:randomBackground}]}>
       <Header/>
-      <GeneratePassword/>
+      <GeneratePassword />
+      <TouchableOpacity onPress={ generateColor} style={styles.backBg}>
+        <Text style={styles.backBgText}>
+          Change Background Color
+        </Text>
+      </TouchableOpacity>
       <Footer/>
     </View>
   )
@@ -23,4 +37,20 @@ const styles = StyleSheet.create({
     // backgroundColor: '#F5F5F5',
     justifyContent: 'space-between',
   },
+  backBg: {
+    backgroundColor: 'white',
+    height: 30,
+    padding:5,
+    marginLeft: 'auto',
+    marginRight: 15,
+    marginBottom: 15,
+    borderRadius: 4,
+    paddingHorizontal:15,
+  },
+  backBgText: {
+    fontWeight: 'bold',
+    flex: 1,
+    justifyContent: 'center',
+    alignItems:'center',
+  }
 });
